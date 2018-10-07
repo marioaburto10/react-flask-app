cdd50c948e9bcee6c83f6d80e6639cfb13087cdd
Mario Aburto <marioaburto10@gmail.com>
2018-10-07 00:09:32 -0400

    flask server now rendering react

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ from flask import Flask, render_template, request, jsonify, Blueprint
 # create instance of Flask app
 app = Flask(__name__, template_folder="client/build", static_folder="client/build/static")
 
-# List of dictionaries
-dogs = [{"name": "Fido", "type": "Lab"},
-        {"name": "Rex", "type": "Collie"},
-        {"name": "Suzzy", "type": "Terrier"},
-        {"name": "Tomato", "type": "Retriever"}]
-
-
 # create route that renders index.html template
 @app.route("/")
 def index():
