5835d33928916e95af17d5eab839b31d36137b04
Mario Aburto <marioaburto10@gmail.com>
2018-10-08 21:28:50 -0400

    added speech to text functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,111 +1,137 @@
-import React, {Component}          from 'react';
-import { render }                  from 'react-dom';
-import { FloatingActionButton,
-        MuiThemeProvider }         from 'material-ui';
-import MicrophoneOn                from 'material-ui/svg-icons/av/mic';
-import MicrophoneOff               from 'material-ui/svg-icons/av/stop';
+'use strict'
+import React, { Component } from "react"
 
-// import { ReactMic, saveRecording } from '/components';
-import ReactMic from './ReactMic';
-// import sampleAudio                 from './sample_audio.webm';
-// import ReactGA                     from 'react-ga';
+//------------------------SPEECH RECOGNITION-----------------------------
 
-// require ('./styles.scss');
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+
+recognition.continous = true
+recognition.interimResults = true
+recognition.lang = 'en-US'
 
 
-// ReactGA.initialize('UA-98862819-1');
+//------------------------COMPONENT-----------------------------
 
-export default class Demo extends Component {
-  constructor(props){
-    super(props);
+class Speech extends Component {
+
+  constructor() {
+    super()
     this.state = {
-      blobObject: null,
-      isRecording: false
+      listening: false
     }
+    this.toggleListen = this.toggleListen.bind(this)
+    this.handleListen = this.handleListen.bind(this)
   }
 
-  componentDidMount() {
-    // ReactGA.pageview(window.location.pathname);
-  }
-
-  startRecording= () => {
+  toggleListen() {
     this.setState({
-      isRecording: true
-    });
+      listening: !this.state.listening
+    }, this.handleListen)
   }
 
-  stopRecording= () => {
-    this.setState({
-      isRecording: false
-    });
-  }
+  handleListen() {
 
-  onSave=(blobObject) => {
-  }
+    console.log('listening?', this.state.listening)
 
-  onStart=() => {
-    console.log('You can tap into the onStart callback');
-  }
+    if (this.state.listening) {
+      recognition.start()
+      recognition.onend = () => {
+        console.log("...continue listening...")
+        recognition.start()
+      }
 
-  onStop= (blobObject) => {
-    this.setState({
-      blobURL : blobObject.blobURL
-    });
-  }
+    } else {
+      recognition.stop()
+      recognition.onend = () => {
+        console.log("Stopped listening per click")
+      }
+    }
 
-  onData(recordedBlob){
-    console.log('chunk of real-time data is: ', recordedBlob);
+    recognition.onstart = () => {
+      console.log("Listening!")
+    }
+
+    let finalTranscript = ''
+    recognition.onresult = event => {
+      let interimTranscript = ''
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+        else interimTranscript += transcript;
+      }
+      document.getElementById('interim').innerHTML = interimTranscript
+      document.getElementById('final').innerHTML = finalTranscript
+
+    //-------------------------COMMANDS------------------------------------
+
+      const transcriptArr = finalTranscript.split(' ')
+      const stopCmd = transcriptArr.slice(-3, -1)
+      console.log('stopCmd', stopCmd)
+
+      if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening'){
+        recognition.stop()
+        recognition.onend = () => {
+          console.log('Stopped listening per command')
+          const finalText = transcriptArr.slice(0, -3).join(' ')
+          document.getElementById('final').innerHTML = finalText
+        }
+      }
+    }
+    
+  //-----------------------------------------------------------------------
+    
+    recognition.onerror = event => {
+      console.log("Error occurred in recognition: " + event.error)
+    }
+
   }
 
   render() {
-    const { isRecording } = this.state;
-
-    return(
-      <MuiThemeProvider>
-        <div>
-          <h1>React-Mic</h1>
-          <p><a href="https://github.com/hackingbeauty/react-mic">Documentation</a></p>
-          <ReactMic
-            className="oscilloscope"
-            record={isRecording}
-            backgroundColor="#FF4081"
-            visualSetting="sinewave"
-            audioBitsPerSecond= {128000}
-            onStop={this.onStop}
-            onStart={this.onStart}
-            onSave={this.onSave}
-            onData={this.onData}
-            strokeColor="#000000" />
-          <div>
-            <audio ref="audioSource" controls="controls" src={this.state.blobURL}></audio>
-          </div>
-          <br />
-          <br />
-          <FloatingActionButton
-            className="btn"
-            secondary={true}
-            disabled={isRecording}
-            onClick={this.startRecording}>
-            <MicrophoneOn />
-          </FloatingActionButton>
-          <FloatingActionButton
-            className="btn"
-            secondary={true}
-            disabled={!isRecording}
-            onClick={this.stopRecording}>
-            <MicrophoneOff />
-          </FloatingActionButton>
-          <br />
-          <br />
-          <br />
-          <p>As featured in the course <br /><a href="http://professionalreactapp.com">How To Develop A Professional React App</a></p>
-          <br />
-          <br />
-          <p>Check out how I use it in my app
-          <br />
-            <a href="http://voicerecordpro.com" target="_blank">Voice Record Pro</a> (record your voice and publish it)</p>
-        </div>
-    </MuiThemeProvider>
-    );
+    return (
+      <div style={container}>
+        <button id='microphone-btn' style={button} onClick={this.toggleListen} />
+        <div id='interim' style={interim}></div>
+        <div id='final' style={final}></div>
+      </div>
+    )
   }
 }
+
+export default Speech
+
+
+//-------------------------CSS------------------------------------
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center'
+  },
+  button: {
+    width: '60px',
+    height: '60px',
+    background: 'lightblue',
+    borderRadius: '50%',
+    margin: '6em 0 2em 0'
+  },
+  interim: {
+    color: 'gray',
+    border: '#ccc 1px solid',
+    padding: '1em',
+    margin: '1em',
+    width: '300px'
+  },
+  final: {
+    color: 'black',
+    border: '#ccc 1px solid',
+    padding: '1em',
+    margin: '1em',
+    width: '300px'
+  }
+}
+
+const { container, button, interim, final } = styles
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Demo from './App';
+import BasicExample from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+ReactDOM.render(<BasicExample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
